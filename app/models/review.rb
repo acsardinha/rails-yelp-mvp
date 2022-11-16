f0272b4e396c8667e95428f1a88a1d91c27d49fe
Andre Sardinha <acsardinha@hotmail.com>
2022-11-16 16:49:00 +0000

    Yelp MVP exercise SOLUTION (NO DESIGN)

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: {
-    in: (0...5).to_a
+    in: 0..5
   }
 end
