1a8f75bef9bf82307e83ae77eecafbd69928831f
Andre Sardinha <acsardinha@hotmail.com>
2022-11-16 16:43:40 +0000

    YELP MVP exercise SOLUTION (NO DESIGN)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, :rating, presence: true
+  validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: {
-    in: 0..5,
-    message: 'Chose a number from 0 to 5'
+    in: (0...5).to_a
   }
 end
